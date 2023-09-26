8e7a61f91efc8e9be9baebd2d1a48c577694d4fc
ziyo <jon81824@gmail.com>
2023-09-26 18:47:55 +0500

    hamma fayillarnigitga qo'shdik

---
+++ bin/najot_talim_crm.dart
@@ -0,0 +1,5 @@
+import 'package:najot_talim_crm/najot_talim_crm.dart' as najot_talim_crm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${najot_talim_crm.calculate()}!');
+}
---
+++ test/najot_talim_crm_test.dart
@@ -0,0 +1,8 @@
+import 'package:najot_talim_crm/najot_talim_crm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
